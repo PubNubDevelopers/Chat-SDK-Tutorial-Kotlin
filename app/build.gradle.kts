e3d713d6e552ec321a7af05c7d0fe4d5874a5111
darryn.campbell <darryn.campbell@pubnub.com>
2024-09-23 07:52:49 +0100

    Upgrade gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.pubnub.chatsdk.kotlintutorial"
-    compileSdk = 34
+    compileSdk = 35
 
         defaultConfig {
         applicationId = "com.pubnub.chatsdk.kotlintutorial"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
